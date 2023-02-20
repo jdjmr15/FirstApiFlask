cafca8d510d814c039088a0225c890f2f1bc96d8
Marquez Rangel <jmarquezr@deloitte.com>
2023-02-19 21:27:33 -0300

    [New] Initial First REST

---
+++ app.py
@@ -0,0 +1,59 @@
+from typing import Dict, List, Any
+from flask import Flask, request
+
+app = Flask(__name__)
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+stores: List[Dict[str, Any]] = [
+    {
+        "name": "My Store",
+        "items": [
+            {
+                "name": "my item",
+                "price": 15.99
+            }
+        ]
+    }
+]
+
+
+@app.get("/store")
+def get_stores():
+    return {"stores": stores}, 200
+
+
+@app.get("/store/<string:name>")
+def get_store(name):
+    for store in stores:
+        if store["name"] == name:
+            return store, 200
+    return {"message": "Store not found.!!"}, 404
+
+
+@app.get("/store/<string:name>/item")
+def get_item(name):
+    for store in stores:
+        if store["name"] == name:
+            return {"items": store["items"]}, 201
+    return {"message": "Store not found.!!"}, 404
+
+
+@app.post("/store")
+def create_store():
+    request_data: Dict[str, Any] = request.get_json()
+    new_store = {"name": request_data["name"], "items": []}
+    stores.append(new_store)
+    return new_store, 201
+
+
+@app.post("/store/<string:name>/item")
+def create_item(name):
+    request_data: Dict[str, Any] = request.get_json()
+
+    for store in stores:
+        if store["name"] == name:
+            new_item: Dict[str, Any] = {"name": request_data["name"],
+                                        "price": request_data["price"]}
+            store["items"].append(new_item)
+            return new_item, 201
+    return {"message": "Store not found.!!"}, 404
