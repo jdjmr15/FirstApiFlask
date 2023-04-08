62522f6b348d431c27f235f27d703e7d15e1dce8
Marquez Rangel <jmarquezr@deloitte.com>
2023-04-07 20:50:32 -0400

    [UPD] Update commit message regarding my first API

--- app.py
+++ app.py
@@ -1,59 +1,66 @@
-from typing import Dict, List, Any
+import uuid
+from typing import Dict, Any
+from db import stores, items
+
+from flask_smorest import abort
 from flask import Flask, request
 
-app = Flask(__name__)
-app.config["TEMPLATES_AUTO_RELOAD"] = True
 
-stores: List[Dict[str, Any]] = [
-    {
-        "name": "My Store",
-        "items": [
-            {
-                "name": "my item",
-                "price": 15.99
-            }
-        ]
-    }
-]
+app = Flask(__name__)
 
 
 @app.get("/store")
 def get_stores():
-    return {"stores": stores}, 200
+    return {"stores": list(stores.values())}, 200
 
 
-@app.get("/store/<string:name>")
-def get_store(name):
-    for store in stores:
-        if store["name"] == name:
-            return store, 200
-    return {"message": "Store not found.!!"}, 404
+@app.get("/store/<string:store_id>")
+def get_store(store_id):
+    try:
+        return stores[store_id]
+    except KeyError:
+        abort(404, message="Store not found.!!")
 
 
-@app.get("/store/<string:name>/item")
-def get_item(name):
-    for store in stores:
-        if store["name"] == name:
-            return {"items": store["items"]}, 201
-    return {"message": "Store not found.!!"}, 404
+@app.get("/item")
+def get_all_items():
+    return {"stores": list(items.values())}, 200
+
+
+@app.get("/item/<string:item_id>")
+def get_item(item_id):
+    try:
+        return items[item_id]
+    except KeyError:
+        abort(404, message="Item not found.!!")
+
+
+@app.delete("/item/<string:item_id>")
+def delete_item(item_id):
+    try:
+        del items[item_id]
+        return {"message": "Item deleted"}
+    except KeyError:
+        abort(404, message="Item not found.!!")
 
 
 @app.post("/store")
 def create_store():
-    request_data: Dict[str, Any] = request.get_json()
-    new_store = {"name": request_data["name"], "items": []}
-    stores.append(new_store)
-    return new_store, 201
+    store_data: Dict[str, Any] = request.get_json()
+    store_id = uuid.uuid4().hex
+    store = {**store_data, "id": store_id}
+    stores[store_id] = store
+    return store, 201
 
 
-@app.post("/store/<string:name>/item")
-def create_item(name):
-    request_data: Dict[str, Any] = request.get_json()
+@app.post("/item")
+def create_item():
+    item_data: Dict[str, Any] = request.get_json()
+    if item_data["store_id"] not in stores:
+        abort(404, message="Store not found.!!")
 
-    for store in stores:
-        if store["name"] == name:
-            new_item: Dict[str, Any] = {"name": request_data["name"],
-                                        "price": request_data["price"]}
-            store["items"].append(new_item)
-            return new_item, 201
-    return {"message": "Store not found.!!"}, 404
+    item_id = uuid.uuid4().hex
+    item = {**item_data, "id": item_id}
+    items[item_id] = item
+
+    return item, 201
---
+++ db.py
@@ -0,0 +1,4 @@
+from typing import Dict, Any
+
+stores: Dict[str, Any] = {}
+items: Dict[str, Any] = {}
